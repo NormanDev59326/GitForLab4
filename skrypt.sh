465aa46e8e8ebffcd462b6b899ea89b1eb864fc4
NormanDev59326 <dsw59326@student.dsw.edu.pl>
2025-05-23 17:27:38 +0200

    Dodano --logs z obsługą parametru

--- skrypt.sh
+++ skrypt.sh
@@ -1,1 +1,9 @@
 [[ "$1" == "--date" || "$1" == "-d" ]] && date
+
+if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+  count=${2:-100}
+  for i in $(seq 1 $count); do
+    filename="log$i.txt"
+    echo -e "$filename\nScript: $0\nDate: $(date)" > "$filename"
+  done
+fi
